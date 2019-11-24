5738965e8e305d4d077c4d8b634b47bfeceba91a
Grayder0152 <58142395+Grayder0152@users.noreply.github.com>
2019-11-24 19:48:46 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		autoplay:true,
+		autoplaySpeed:5000,
+	    adaptiveHeight: true,
+	    prevArrow: '',
+	    nextArrow: ''
+	})
+	$('.button_mob_menu').click(function(){
+		$(this).toggleClass('active_button_mob_menu')
+		$('.button_mob_menu span').toggleClass('rotate')
+		$('.mob_menu').slideToggle(1000)
+	})
+})
